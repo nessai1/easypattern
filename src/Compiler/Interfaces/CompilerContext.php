f161d3704daf9a2fcae54e562fd2241d268f7201
Daniil Verevkin <werowkin@gmail.com>
2023-01-04 18:22:19 +0700

    Compiler: main interfaces

---
+++ src/Compiler/Interfaces/CompilerContext.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Verevkin\EasyPattern\Compiler\Interfaces;
+
+use Verevkin\EasyPattern\Template\Interfaces\TemplateParamsContainer;
+
+/**
+ * Context of compiler that involved in inner text compilation and contain variables
+ *
+ * @see Compiler
+ */
+interface CompilerContext extends TemplateParamsContainer
+{}
